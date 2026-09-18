b7706da8f3d725c8ca938cea56aef1c48ee4ebc2
stefanrogic <stefanrogic@protonmail.com>
2026-09-18 18:54:33 +0200

    fixed xp bug

--- utils/xp.py
+++ utils/xp.py
@@ -3,11 +3,12 @@ from time import time
 
 XP_PER_LEVEL = 1000
 MAX_LEVEL = 30
+MAX_XP = XP_PER_LEVEL * (MAX_LEVEL - 1)
 XP_PER_KEK = 1  # $convert rate: 10 xp -> 1 kek, one-way only
 
 
 def get_level(xp):
-    return min(xp // XP_PER_LEVEL + 1, MAX_LEVEL)
+    return min(xp, MAX_XP) // XP_PER_LEVEL + 1
 
 
 def get_kek_bonus(level):
@@ -24,7 +25,7 @@ def record_level_change(user, previous_xp, timestamp=None):
 
 def add_xp(user, amount):
     before = user.get("xp", 0)
-    user["xp"] = min(before + amount, XP_PER_LEVEL * MAX_LEVEL)
+    user["xp"] = min(before + amount, MAX_XP)
     record_level_change(user, before)
     return user["xp"] - before
 
